1708cc32e4431b3d9b5fd3cbe40b3bd9836ee412
Rob Ranf <rob@emiyaconsulting.com>
2024-12-12 15:10:36 -0800

    Initial Commit

---
+++ DramaPop/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DramaPop
+//
+//  Created by Rob Ranf on 12/12/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
